76eff22786d4f3e51b6ec40f96feb32da378c0ad
drecc4 <80606130+drecc4@users.noreply.github.com>
2022-04-01 08:21:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -347,23 +347,6 @@ st.markdown(' ')
 #-------------------------------------------------------------------------------------------------------------------------------
 
 
-#Testing
-
-export_as_pdf = st.button("Export Report")
-
-def create_download_link(val, filename):
-    b64 = base64.b64encode(val)  # val looks like b'...'
-    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
-
-if export_as_pdf:
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font('Arial', 'B', 16)
-    pdf.cell(80, 20, "Test")
-    
-    html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
-
-    st.markdown(html, unsafe_allow_html=True)
 
 
 
